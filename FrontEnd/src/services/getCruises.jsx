2d61317e8403115655e55af3404d3ffe00d38b93
GabrielCostaMarques <gabrielcostamarques2@gmail.com>
2025-11-21 17:06:03 -0300

    chore: add link api

--- FrontEnd/src/services/getCruises.jsx
+++ FrontEnd/src/services/getCruises.jsx
@@ -1,6 +1,6 @@
 
 export async function getCruises() {
-    const result = await fetch('/public/data/ofertasAzamara.json');
+    const result = await fetch('https://manualdoagente.com.br/wp-content/uploads/ofertasAzamara.json');
     const data = await result.json();
     return data
 }
