3be29b8e0eb4216d646bcafe1a37fe2f6f74395f
GabrielCostaMarques <gabrielcostamarques2@gmail.com>
2025-12-01 13:51:44 -0300

    chore: largura banner principal

--- FrontEnd/src/services/getCruises.jsx
+++ FrontEnd/src/services/getCruises.jsx
@@ -1,7 +1,18 @@
-
 export async function getCruises() {
-    const result = await fetch('https://manualdoagente.com.br/wp-json/custom/v1/ofertas-azamara');
-    const data = await result.json();
-    return data
-}
+  const username = "Gabriel Marques";
+  const appPassword = "Yerm ri9e Bo4J xpyB TD5g 548E";
 
+  const token = btoa(`${username}:${appPassword}`.trim());
+
+  const result = await fetch(
+    "https://manualdoagente.com.br/wp-json/custom/v1/ofertas-azamara",
+    {
+      headers: {
+        Authorization: `Basic ${token}`,
+      },
+    }
+  );
+
+  const data = await result.json();
+  return data;
+}
